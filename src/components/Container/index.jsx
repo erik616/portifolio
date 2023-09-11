9526791eb0e540bc5d3c5618728beef3e60e06dd
Erik D <erikjhow.ed@gmail.com>
2023-09-11 15:07:28 -0300

    changes

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import style from "./style.module.css";
 
+import arrow from "../../assets/arrowback.svg";
+
 export function Container({ children, title }) {
+  const [visible, setVisible] = useState(false);
+
+  const setVisibleButton = () => {
+    const windowLocale = document.documentElement.scrollTop;
+
+    if (windowLocale >= 100) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  const handleGetTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
+  
+  window.addEventListener("scroll", setVisibleButton);
   return (
     <section className={style.galary}>
       {title ? <h1>{title}</h1> : null}
       <div className={style.galary_container}>{children}</div>
+      {visible ? (
+        <button className={style.buttontop} onClick={handleGetTop}>
+          <img src={arrow} />
+        </button>
+      ) : null}
     </section>
   );
 }
